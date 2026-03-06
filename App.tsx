850446b46f28948b1d09ec212f8559e75964fa14
Basem Al-Hersh <alhrshbasm4@gmail.com>
2026-03-06 21:37:56 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,9 +4,9 @@ import { StatusBar } from "expo-status-bar";
 import * as SplashScreen from "expo-splash-screen";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useFonts } from "expo-font";
-
+import { databaseService } from './src/services/SQLiteService';
 import { MushafScreen } from "./src/screens/MushafScreen";
-
+import { SearchScreen } from "./src/screens/SearchScreen";
 SplashScreen.preventAutoHideAsync().catch(() => undefined);
 
 SplashScreen.setOptions({ fade: true, duration: 1000 });
@@ -15,17 +15,30 @@ export default function App() {
   const [fontsLoaded, fontError] = useFonts({
     uthmanTn1Bold: require("./assets/fonts/UthmanTN1B-Ver20.ttf"),
   });
-
+  
   useEffect(() => {
     if (fontsLoaded || fontError) {
       void SplashScreen.hideAsync();
     }
   }, [fontsLoaded, fontError]);
-
+  
   if (fontError) {
     throw fontError;
   }
+  
+  useEffect(() => {
+  const initializeApp = async () => {
+    try {
+      const db = await databaseService.getDb();
+      await databaseService.initializeFTS5Table(db);
+      await databaseService.populateFTS5Table(db);
+    } catch (error) {
+      console.error('Error initializing app:', error);
+    }
+  };
 
+  initializeApp();
+}, []);
   if (!fontsLoaded) {
     return (
       <View style={styles.loader}>
@@ -37,7 +50,7 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="dark" />
-      <MushafScreen />
+      <SearchScreen />
     </SafeAreaView>
   );
 }
